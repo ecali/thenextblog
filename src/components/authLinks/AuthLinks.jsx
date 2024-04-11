bf5b0a67a8a5a6628750907806d85c9bee374900
ecali <78408765+ecali@users.noreply.github.com>
2024-04-11 15:45:10 +0200

    add more components for navbar

---
+++ src/components/authLinks/AuthLinks.jsx
@@ -0,0 +1,7 @@
+import styles from './authLinks.module.css'
+
+export const AuthLinks = () => {
+    return(
+        <div className={styles.container}>Auth Links</div>
+    )
+}
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,6 @@
+import styles from './themeToggle.module.css'
+export const ThemeToggle = () => {
+    return (
+        <div className={styles.container}>Theme Toggle</div>
+    )
+}
