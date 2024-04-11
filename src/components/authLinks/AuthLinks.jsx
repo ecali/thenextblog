50f37794c3e064960a8ce2596262d9a223dd6703
ecali <78408765+ecali@users.noreply.github.com>
2024-04-11 16:51:43 +0200

    add mobile menu and burger

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,20 +1,43 @@
+'use client'
+
 import styles from './authLinks.module.css'
 import Link from "next/link";
+import {useState} from "react";
 
 export const AuthLinks = () => {
-
+    const [open, setOpen] = useState(false)
     //temp
     const status = 'notathenticated'
     return(
         <>
             {status === 'notathenticated' ?
-                <Link href={'/login'}>Login</Link> :
+                <Link href={'/login'} className={styles.link}>Login</Link> :
                 <>
-                    <Link href={'/write'}>Write</Link>
+                    <Link href={'/write'} className={styles.link}>Write</Link>
                     <span className={styles.link}>Logout</span>
                 </>
 
             }
+            <div className={styles.burger} onClick={() => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            {open && (
+                <div className={styles.responsiveMenu}>
+                    <Link href={'/'}>Homepage</Link>
+                    <Link href={'/'}>About</Link>
+                    <Link href={'/'}>Contact</Link>
+                    {status === 'notathenticated' ?
+                        <Link href={'/login'}>Login</Link> :
+                        <>
+                            <Link href={'/write'}>Write</Link>
+                            <span className={styles.link}>Logout</span>
+                        </>
+
+                    }
+                </div>
+            )}
         </>
     )
 }
